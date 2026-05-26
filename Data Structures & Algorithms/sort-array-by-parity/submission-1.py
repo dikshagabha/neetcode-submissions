faaaf07c6fcd209ad1bf235cc01f43828d9bdf07
diksha <diksha.gabha95@gmail.com>
2026-05-26 12:21:05 +0530

    Add: sort-array-by-parity - submission-1

---
+++ Data Structures & Algorithms/sort-array-by-parity/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        if len(nums)<1:
+            return nums
+
+        l, r = 0, len(nums)-1
+        while l<r:
+            while l<r and nums[l] %2 ==0:
+                l+=1
+            while l<r and nums[r]%2 != 0:
+                r-=1
+            
+            nums[l], nums[r] = nums[r], nums[l]
+            r-=1
+            l+=1
+        return nums
+
