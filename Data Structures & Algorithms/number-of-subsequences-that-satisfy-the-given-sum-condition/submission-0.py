5383acab7ca3a0d21ea45da71208a5f87ec27b8b
diksha <diksha.gabha95@gmail.com>
2026-06-26 12:12:48 +0530

    Add: number-of-subsequences-that-satisfy-the-given-sum-condition - submission-0

---
+++ Data Structures & Algorithms/number-of-subsequences-that-satisfy-the-given-sum-condition/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        res = 0
+        mod = 10**9 + 7
+
+
+        r=len(nums)-1
+        for l in range(0, len(nums)):
+            #if nums[l]+nums[r]<target:
+            while l<=r and nums[l]+nums[r]>target:
+                r-=1
+            if l<=r:
+                res += pow(2, r-l, mod)
+                res %= mod
+        return res
