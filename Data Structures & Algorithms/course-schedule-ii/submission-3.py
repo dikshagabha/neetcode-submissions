eb8cdba0e5bea762130b1b8f6d9513962159ee97
diksha <diksha.gabha95@gmail.com>
2026-07-29 11:54:19 +0530

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        
+        nei = {}
+        for i in range(numCourses):
+            nei[i] = []
+        for src, dest in prerequisites:
+            nei[src].append(dest)
+        #print(nei)
+        path = set()
+        visited=set()
+        res = []
+        def detect_cycle(i):
+            if i in visited:
+                return False
+            if i in path:
+                return True
+            
+            visited.add(i)
+            for req in nei[i]:
+                if detect_cycle(req)==False:
+                    return False
+            visited.remove(i)
+            path.add(i)
+            res.append(i)
+            return True
+
+        for i in range(numCourses):
+            if detect_cycle(i)==False:
+                return []
+        res += [i for i in range(max(res)+1, numCourses)]
+        return res
